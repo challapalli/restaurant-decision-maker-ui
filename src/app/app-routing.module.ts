64ea39fa169f533659e96c34786351119cdea181
user <chandu.eee.03@gmail.com>
2024-04-04 10:27:56 +0800

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SessionCreateComponent } from './session-create/session-create.component';
+import { SessionJoinComponent } from './session-join/session-join.component';
+import { RestaurantSubmitComponent } from './restaurant-submit/restaurant-submit.component';
+import { SessionEndComponent } from './session-end/session-end.component';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './helpers/auth.guard';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'create-session', component: SessionCreateComponent, canActivate: [AuthGuard] },
+  { path: 'join-session', component: SessionJoinComponent, canActivate: [AuthGuard] },
+  { path: 'submit-restaurant/:sessionId/:username', component: RestaurantSubmitComponent , canActivate: [AuthGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
